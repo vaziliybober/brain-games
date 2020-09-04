aabab540eaddc7bd0e3f893b5026cd329fb3fe4e
vaziliybober <vaziliybober@gmail.com>
2020-09-04 22:27:45 +0300

    refactor calc.js module

--- src/games/calc.js
+++ src/games/calc.js
@@ -2,8 +2,9 @@ import runGame from '../index.js';
 import { genRandInt, chooseRandElem } from '../util.js';
 
 const taskDescription = 'What is the result of the expression?';
+const operatorSigns = ['+', '-', '*'];
 
-const parseOperator = (string) => {
+const getOperatorBySign = (string) => {
   if (string === '+') {
     return (a, b) => a + b;
   }
@@ -24,11 +25,12 @@ const buildQuestionAndAnswer = () => {
   const to = 100;
   const operand1 = genRandInt(from, to);
   const operand2 = genRandInt(from, to);
-  const operatorSign = chooseRandElem(['+', '-', '*']);
+  const operatorSign = chooseRandElem(operatorSigns);
+  const operator = getOperatorBySign(operatorSign);
 
   return {
     question: `${operand1} ${operatorSign} ${operand2}`,
-    answer: String(parseOperator(operatorSign)(operand1, operand2)),
+    answer: String(operator(operand1, operand2)),
   };
 };
 
